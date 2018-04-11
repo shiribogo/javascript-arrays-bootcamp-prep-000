a0f37e92dc210f91a42321accdb11e2ea46c80e4
Shiri Mund <shiribogo@gmail.com>
2018-04-11 01:12:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,5 +35,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array){
-return array.slice(0,array.length-1)
+  array.slice(0,array.length-1)
+  return array
 }
