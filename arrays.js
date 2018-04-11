1deda2dbea8266065a465c06ce927ad89724de42
Shiri Mund <shiribogo@gmail.com>
2018-04-11 00:47:46 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,39 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(a, b){
+ ["b",...a]
+}
+function destructivelyAddElementToBeginningOfArray(a, b){
+   a.unshift(b)
+}
+
+function addElementToEndOfArray(a, b){
+ [a,..."b"]
+}
+function destructivelyAddElementToEndOfArray(a, b){
+   a.push(b)
+}
+
+function accessElementInArray(a, b){
+ var c = console.log(a[b])
+ return c
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(a) {
+  a.shift()
+  return a
+}
+function removeElementFromBeginningOfArray(a){
+a.slice(0)
+return a
+}
+
+function destructivelyRemoveElementFromEndOfArray(a) {
+  a.pop()
+  return a
+}
+
+function removeElementFromEndOfArray(a){
+a.slice(0,a.length-1)
+return a
+}
