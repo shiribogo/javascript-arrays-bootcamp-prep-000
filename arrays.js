4f5032d43425182584a5c30ee079a41eaf0c4b86
Shiri Mund <shiribogo@gmail.com>
2018-04-11 01:12:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,8 +25,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array
 }
 function removeElementFromBeginningOfArray(array){
-  array.slice(1)
-  return array
+  return array.slice(1)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
